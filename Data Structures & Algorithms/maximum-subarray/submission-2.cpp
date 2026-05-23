6b91f7218aca29cfff96fca8964dedff36ea02ec
Ashwin Guda <91225319+AshwinRG1@users.noreply.github.com>
2026-05-22 23:50:26 -0400

    Add: maximum-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-subarray/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp = nums;
+        int best = nums[0];
+
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++){
+            dp[i] = max(dp[i-1] + dp[i], dp[i]);
+            best = max(best, dp[i]);
+        }
+        return dp[nums.size() - 1];
+    }
+};
