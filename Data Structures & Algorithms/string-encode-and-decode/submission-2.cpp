b15e689ad3d6bcca1d4d31e3e0f0b59eb7f12e13
Ashwin Guda <91225319+AshwinRG1@users.noreply.github.com>
2026-06-06 13:52:14 -0400

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string res = "";
+        for (string str : strs){
+            int los = str.length();
+            res.append(to_string(los));
+            res += "$";
+            res.append(str);
+        }
+
+        return res;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> res;
+        int i = 0;
+        while (i < s.length()) {
+            int delimiter = s.find("$", i);
+            int length = stoi(s.substr(i, delimiter - i));
+            i = delimiter + 1; 
+            res.push_back(s.substr(i, length));
+            i += length; 
+        }
+        return res;
+        
+    }
+};
